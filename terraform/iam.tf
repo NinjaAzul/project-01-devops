8413adbcd06adc87db5e29b631653237077d7de6
Erick Freitas <erick.freitas@trin.ca>
2025-01-15 10:32:27 -0300

    fix terraform infra

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,72 +16,43 @@ resource "aws_iam_role" "github-actions-role" {
   name = "github-actions-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
         Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:eusouodaniel/rocketseat.ci.iac:ref:refs/heads/main"
+          }
+        }
+        Effect = "Allow"
         Principal = {
           Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
         }
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = [
-              "sts.amazonaws.com"
-            ]
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:NinjaAzul/project-01-devops:ref:refs/heads/main"
-            ]
-          }
-        }
       }
     ]
-
-
+    Version = "2012-10-17"
   })
 
   inline_policy {
-    name = "github-actions-policy"
+    name = "tf-permissions"
     policy = jsonencode({
-      Version = "2012-10-17"
       Statement = [{
         Sid      = "Statement1"
+        Action   = "ecr:*"
         Effect   = "Allow"
-        Action   = "apprunner:*"
         Resource = "*"
         },
         {
-          Sid = "Statement2"
-          Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken",
-            "ecr:DescribeImages",
-            "ecr:ListImages",
-            "ecr:DescribeRepositories",
-            "ecr:GetRepositoryPolicy",
-            "ecr:GetLifecyclePolicy",
-            "ecr:GetLifecyclePolicyPreview",
-            "ecr:ListTagsForResource",
-            "ecr:DescribeImageScanFindings"
-          ]
+          Sid      = "Statement2"
+          Action   = "iam:*"
           Effect   = "Allow"
           Resource = "*"
         },
         {
-          Sid    = "Statement3"
-          Effect = "Allow"
-          Action = [
-            "iam:PassRole",
-            "iam:CreateServiceLinkedRole",
-            "iam:GetRole",
-            "iam:ListRoles"
-          ]
+          Sid      = "Statement3"
+          Action   = "s3:*"
+          Effect   = "Allow"
           Resource = "*"
         }
       ]
@@ -92,23 +63,89 @@ resource "aws_iam_role" "github-actions-role" {
   }
 }
 
+
 resource "aws_iam_role" "app-runner-role" {
-  name = "app-runner-role"
+  name = "app-runner-role" #
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow",
         Principal = {
-          Service = "build.apprunner.amazonaws.com"
+          "Service" = "build.apprunner.amazonaws.com"
         }
         Action = "sts:AssumeRole"
       }
     ]
   })
   managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
   ]
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:sub" = "repo:eusouodaniel/rocketseat.ci.api:ref:refs/heads/main"
+          }
+        }
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+
+  inline_policy {
+    name = "ecr-app-permissions"
+    policy = jsonencode({
+      Statement = [{
+        Sid      = "Statement1"
+        Action   = "apprunner:*"
+        Effect   = "Allow"
+        Resource = "*"
+        },
+        {
+          Sid = "Statement2"
+          Action = [
+            "iam:PassRole",
+            "iam:CreateServiceLinkedRole",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+        {
+          Sid = "Statement3"
+          Action = [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        }
+      ]
+    })
+  }
+
   tags = {
     IAC = "True"
   }
