d66e080dcfa562f165f9cac7866e380f00386156
Erick Freitas <erick.freitas@trin.ca>
2025-01-15 11:29:16 -0300

    fix tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,28 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
-  let appService: AppService;
 
   beforeEach(async () => {
-    const mockAppService = {
-      getHello: jest.fn().mockResolvedValue('Data from database'),
-    };
-
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [{ provide: AppService, useValue: mockAppService }],
     }).compile();
 
     appController = app.get<AppController>(AppController);
-    appService = app.get<AppService>(AppService);
   });
 
-  it('should return data from the database', async () => {
-    const result = await appController.getHello();
-    expect(result).toBe('Data from database');
-    expect(appService.getHello).toHaveBeenCalled();
+  it('deve retornar Hello World!', () => {
+    const result = appController.getHello();
+    expect(result).toBe('Hello World!');
   });
 });
