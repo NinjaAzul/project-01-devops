3e1d6a91fef6bb2de49cfcfa510b001b6a5670b2
Erick Freitas <erick.freitas@trin.ca>
2025-01-27 03:11:38 -0300

    update

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "devops-ci" {
   name                 = "devops-ci"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
